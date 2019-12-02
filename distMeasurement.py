b1d68a15ebfe1ac8cb598da326c58e8b3547943c
Tyler Anderson <twa16@case.edu>
2019-12-02 10:39:27 -0500

    Whitespace changes

--- distMeasurement.py
+++ distMeasurement.py
@@ -9,14 +9,14 @@ DESTINATION_PORT_NUM = 33434
 
 def do_ips_match(ip_string, ip_array):
     ip_num_array = ip_string.split('.')
-    
+
     for i in range(4):
         if int(ip_num_array[i]) != ip_array[i]:
             return False
-    
+
     return True
-        
-        
+
+
 def do_ports_match(port_int, port):
     return port_int == port
 
@@ -44,10 +44,10 @@ if __name__ == "__main__":
     for destination in destinations:
         attempts = 0
         read_response = False
-        
+
         while attempts < 3 and not read_response:
             destination_ip_address = 0
-            
+
             try:
                 destination_ip_address = socket.gethostbyname(destination)
             except socket.gaierror:
@@ -68,7 +68,7 @@ if __name__ == "__main__":
             # Send socket
             send_sock.sendto(payload, (destination_ip_address, DESTINATION_PORT_NUM))
             time_sent = time.time()
-            
+
             # Create raw socket to receive ICMP messages
             recv_sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
 
@@ -87,35 +87,35 @@ if __name__ == "__main__":
                 rtt = time.time() - time_sent         
                 print(f'Destination: {destination}')
                 print(f'Destination ip address: {destination_ip_address}')
-            
+
                 # Get ICMP packet
                 icmp_packet = recv_sock.recv(2000)  # should be plenty
                 read_response = True
                 print(f'Length of packet: {len(icmp_packet)}')
-                
+
                 # Type
                 icmp_type_start_index = 20
                 icmp_type_end_index = 21
                 icmp_type = ord(icmp_packet[icmp_type_start_index:icmp_type_end_index])
                 print(f'Type: {icmp_type}')
                 right_type = is_right_type(icmp_type)
-                
+
                 # Code
                 code_start_index = 21
                 code_end_index = 22
                 code = ord(icmp_packet[code_start_index:code_end_index])
                 print(f'Code: {code}')
                 right_code = is_right_code(code)
-                
+
                 right_type_and_code = right_type and right_code
-                
+
                 # Time to Live
                 time_to_live_start_index = 36
                 time_to_live_end_index = 37
                 time_to_live = ord(icmp_packet[time_to_live_start_index:time_to_live_end_index])
                 print(f'Time_to_live: {time_to_live}')
                 num_hops = ttl - time_to_live
-                
+
                 # Check if match:
                 dest_address_start_index = 16 + 28
                 dest_address_end_index = 20 + 28
@@ -140,7 +140,7 @@ if __name__ == "__main__":
                 print(f'Ports match: {matched_destination_port}')
 
                 print(f'Num of matches: {num_matched}')
-                
+
                 print(f'Num hops: {num_hops}')
                 print(f'RTT in ms: {rtt*1000}')
 
